2ee7944b90e8a4b1735e82847950f554c26ad745
William C. Canin <william.costa.canin@gmail.com>
2019-12-29 04:15:15 -0300

    Update

---
+++ tkcrud/data/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clients (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT(200),
+	address TEXT(100),
+	date_of_birth TEXT(13),
+	sex TEXT(15),
+	marital_status TEXT(20),
+	rg TEXT(18),
+	cpf TEXT(15),
+	cell_phone TEXT(17),
+	email TEXT(100)
+);
+CREATE TABLE sqlite_sequence(name,seq);
